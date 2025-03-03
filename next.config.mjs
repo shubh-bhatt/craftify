12ae8e3b7f2e618377d9f7f084d855511bbdc275
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-03-03 10:41:15 +0530

    Updated next.config.js to use images.remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn-icons-png.flaticon.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+      },
+    ],
   },
 };
 
